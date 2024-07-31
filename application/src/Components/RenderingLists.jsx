ae23c655dee36e53e9d838f6cfcd75a396375f60
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2024-07-31 15:56:05 +0500

    s

--- application/src/Components/RenderingLists.jsx
+++ application/src/Components/RenderingLists.jsx
@@ -1,10 +1,13 @@
-const arr = ["abdullah: rizwan", "ahmad: farid", "hammad: doctor"];
-
-
+import data from "../array/people.json";
 
 //Generating a list of items from an array
 function RenderingLists() {
-  const listItems = arr.map((banda) => <li>{banda}</li>);
+  const listItems = data.map((person) => 
+    (
+    <li key={person.id}>
+      {person.name}: {person.profession}
+    </li>
+  ));
   return <ul>{listItems}</ul>;
 }
 
