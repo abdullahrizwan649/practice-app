8ba349279a68991d3a273d511fa8e0ecaef46f87
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2024-07-31 19:14:29 +0500

    "Filter data by profession 'chemist' before mapping to list items"

--- application/src/Components/RenderingLists.jsx
+++ application/src/Components/RenderingLists.jsx
@@ -2,10 +2,11 @@ import data from "../array/people.json";
 
 //Generating a list of items from an array
 function RenderingLists() {
-  const listItems = data.map((person) => 
-    (
+  const chemists = data.filter((person) => person.profession === "chemist");
+
+  const listItems = chemists.map((person) => (
     <li key={person.id}>
-      {person.name}: {person.profession}
+      {person.name} : {person.profession}
     </li>
   ));
   return <ul>{listItems}</ul>;
