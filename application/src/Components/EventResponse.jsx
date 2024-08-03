5182db00a5037d65a97e1b17cd559c42dbb3bee4
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2024-08-03 16:47:06 +0500

    "Refactor EventResponse component to use FormSubmit and handleClick functions"

--- application/src/Components/EventResponse.jsx
+++ application/src/Components/EventResponse.jsx
@@ -1,9 +1,27 @@
-export default function EventResponse() 
-{
-  function handleClick() {
-    alert("CLICKED");
-  }
-
-  return <button onClick={() => alert('You clicked me!')}>WOW</button>
-
+export default function EventResponse() {
+  return (
+    <>
+      <FormSubmit funct={handleClick}>WOW</FormSubmit>
+      {/* <AlertButton funct={handleClick}>GREAT</AlertButton> */}
+    </>
+  );
 }
+
+// function AlertButton({ funct, children }) {
+//   return <button onClick={funct}>{children}</button>;
+// }
+
+function handleClick(e) {
+  e.preventDefault(); //prevents from refreshing after submitting
+  alert("CLICKED");
+}
+
+function FormSubmit({ funct, children }) {
+  return (
+    <form onSubmit={funct}>
+      <input />
+      <button>{children}</button>
+    </form>
+  );
+}
+//children is a special prop jbtw
