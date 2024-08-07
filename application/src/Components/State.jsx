f09adc8f4de01c1d9858e2b40c63b08962bd3269
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2024-08-07 23:04:37 +0500

    "Commented out entire StateUse function and added new empty StateUse function declaration"

--- application/src/Components/State.jsx
+++ application/src/Components/State.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import data from "../array/people.json";
 
-export default function StateUse() {
-  const [age, setAge] = useState(17);
+// export default function StateUse() {
+//   const [age, setAge] = useState(17);
 
-  const handleClick = () => {
-    setAge((prevAge) => prevAge + 1);
-  };
+//   const handleClick = () => {
+//     setAge((prevAge) => prevAge + 1);
+//   };
 
-  return (
-    <>
-      <h1>HEY, I am {age}</h1>
-      <button onClick={handleClick}>Update Age</button>
-      <h2>HEY, I am {age} </h2>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>HEY, I am {age}</h1>
+//       <button onClick={handleClick}>Update Age</button>
+//       <h2>HEY, I am {age} </h2>
+//     </>
+//   );
+// }
+
+export default function StateUse() {}
