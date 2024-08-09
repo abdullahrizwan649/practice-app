f7b792515beb1d41fd073299738b596e27264e76
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2024-08-09 14:53:58 +0500

    state with array

--- application/src/Components/State.jsx
+++ application/src/Components/State.jsx
@@ -17,7 +17,35 @@ import data from "../array/people.json";
 //   );
 // }
 
-export default function StateUse() 
-{
- const [] ;
+const filamentArray = [
+  { brand: "Bambu", temperature: "220", color: "red" },
+  { brand: "Jamg He", temperature: "210", color: "yellow" },
+];
+
+export default function StateUse() {
+  const [filament, setFilament] = useState({
+    brand: "Creality",
+    temperature: "210",
+    color: "black",
+  });
+
+  const updateFilament = (brand) => {
+    const newFilament = filamentArray.find(
+      (filament) => filament.brand === brand
+    );
+    if (newFilament) {
+      setFilament(newFilament);
+    }
+  };
+
+  return (
+    <>
+      <h1>FILAMENT DETAILS</h1>
+      <h2>
+        It is a {filament.color} filament from {filament.brand}.
+      </h2>
+      <button onClick={() => updateFilament("Bambu")}>BAMBU</button>
+      <button onClick={() => updateFilament("Jamg He")}>JAMG HE </button>
+    </>
+  );
 }
